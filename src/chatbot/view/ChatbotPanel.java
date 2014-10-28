de5e276cb089b88cf4151a57c68c5e6f2929dc2d
KostaSergakis27 <kostasergakis27@gmail.com>
2014-10-28 13:46:31 -0600

    edited some gui
    made the scroll bar auto go to the bottom

--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -3,11 +3,14 @@ package chatbot.view;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.*;
 
 import chatbot.controller.ChatbotAppController;
+
 import java.awt.event.KeyAdapter;
 
 public class ChatbotPanel extends JPanel
@@ -19,19 +22,20 @@ public class ChatbotPanel extends JPanel
 	private SpringLayout baseLayout;
 	private JTextArea chatArea;
 	private JScrollPane chatPane;
+	private String startMessage;
 	
 	public ChatbotPanel(ChatbotAppController baseController)
 	{
 		this.baseController = baseController;
 		
+		startMessage = "Welcome to the Derf chatbot. What is your name?";
 		firstButton = new JButton("Click the button... it is so clicky :D");
 		firstTextField = new JTextField(25);
 		baseLayout = new SpringLayout();
 		chatArea = new JTextArea(5, 20);
 		chatPane = new JScrollPane(chatArea);
 		
-		
-		
+		chatArea.setText(startMessage);
 		
 		setupPane();
 		setupPanel();
@@ -44,6 +48,8 @@ public class ChatbotPanel extends JPanel
 	{
 		chatArea.setLineWrap(true);
 		chatArea.setWrapStyleWord(true);
+		
+		
 	}
 	/**
 	 * sets up the panel with the layout options and a button and text field
@@ -65,10 +71,15 @@ public class ChatbotPanel extends JPanel
 	 */
 	private void setupLayout()
 	{
+		//buttonLayout
 		baseLayout.putConstraint(SpringLayout.SOUTH, firstButton, -10, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.EAST, firstButton, -10, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, chatPane, 55, SpringLayout.NORTH, this);
+		//ChatPane Layout
 		baseLayout.putConstraint(SpringLayout.WEST, chatPane, 20, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, chatPane, 20, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, chatPane, -60, SpringLayout.NORTH, firstTextField);
+		baseLayout.putConstraint(SpringLayout.EAST, chatPane, 380, SpringLayout.WEST, this);
+		//firstTextField Layout
 		baseLayout.putConstraint(SpringLayout.SOUTH, firstTextField, -6, SpringLayout.NORTH, firstButton);
 		baseLayout.putConstraint(SpringLayout.EAST, firstTextField, 0, SpringLayout.EAST, firstButton);
 	}
@@ -81,21 +92,29 @@ public class ChatbotPanel extends JPanel
 		{
 			public void actionPerformed(ActionEvent click)
 			{
-				chatArea.append("\n" + (firstTextField.getText()));
+				chatArea.append("\n You:" + (firstTextField.getText()));
 				firstTextField.setText("");
 			}
 		});
 		
 		firstTextField.addKeyListener(new KeyAdapter() 
 		{
-			public void keyPressed(KeyEvent e) 
+			public void keyPressed(KeyEvent event) 
 			{
-				if(e.getKeyCode() == KeyEvent.VK_ENTER)
+				if(event.getKeyCode() == KeyEvent.VK_ENTER)
 				{
-					chatArea.append("\n" + (firstTextField.getText()));
+					chatArea.append("\nYou: " + (firstTextField.getText()));
 					firstTextField.setText("");
 				}
 			}
 		});
+		 
+		chatPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener()  
+		{  
+			public void adjustmentValueChanged(AdjustmentEvent e)  
+			{  
+				chatArea.select(chatArea.getCaretPosition()*12 , chatArea.getText().length());  
+			}  
+		});  
 	}
 }
